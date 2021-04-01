ea4cd16648879768bff952dfb7a62b487dd1b60b
filipe <filipborgs48@gmail.com>
2021-03-31 23:06:21 -0300

    Separando e contando palavras

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const fs = require('fs');
 const f = require('./functions');
+const result = {}
 
-
-// 
 async function main() {
-    // console.log(getSrts);
-    readDir('./legendas').then(f.getSrts).then(console.log)
+    //lendo diretorio
+    readDir('./legendas').then(f.getSrts).then(readLines)
+
+}
+function readLines(arr) {
+    for (const fileName of arr) {
+        //lendo arquivos
+        const patch = `./legendas/${fileName}`;
+        const file = fs.readFileSync(patch).toString();
+
+        //removendo tags
+        palavras = file.replace(/<[a-z="0-9 #]+>|<\/[a-z="0-9 #]+>/gi, '');
+        //separando palavras
+        palavras = palavras.match(/[a-z]+'?[a-z]+/gi);
+        
+        //contando palavras
+        for (let key of palavras) {
+            key = key.toLowerCase();
+            result[key] = result[key] > 0 ? ++result[key] : 1;
+        }
+    }
+    fs.writeFileSync('palavras.json', JSON.stringify(result))
 
 }
 
